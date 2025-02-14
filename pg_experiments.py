fb7a22882815314b4ceac5a8049e756775849a01
Matin Jaffar Aghaei <m.jaffaraghaei@gmail.com>
2025-02-13 19:45:59 -0800

    Increase TIME_TO_LOG to 100

--- pg_experiments.py
+++ pg_experiments.py
@@ -11,7 +11,7 @@ from bandit_environments import Bandit
 from experiment import run_experiment
 
 T = 1_000_000
-TIME_TO_LOG = 10
+TIME_TO_LOG = 100
 NUM_ARMS = 10
 LOG_DIR = f"logs"
 EXP_NAME = f"bad_init"
@@ -47,30 +47,30 @@ environment_definitions = [
 L = 5 / 2
 
 algos = [
-    # {
-    #     "algo_name": "det_pg",
-    #     "algo_kwargs": {"eta": 1 / L},
-    # },
-    # {
-    #     "algo_name": "det_pg_entropy",
-    #     "algo_kwargs": {
-    #         "tau": 0.1
-    #     },  # step-size depends on `L^tau` which requires knowledge of number of arms and is computed later
-    # },
+    {
+        "algo_name": "det_pg",
+        "algo_kwargs": {"eta": 1 / L},
+    },
+    {
+        "algo_name": "det_pg_entropy",
+        "algo_kwargs": {
+            "tau": 0.1
+        },  # step-size depends on `L^tau` which requires knowledge of number of arms and is computed later
+    },
     {
         "algo_name": "det_pg_decaying_entropy",
         "algo_kwargs": {
             "alpha": 1.0
         },  # step-size and tau are computed later
     },
-    # {
-    #     "algo_name": "det_pg_entropy_multistage",
-    #     "algo_kwargs": {
-    #         "tau": 0.1,
-    #         "p": 1,
-    #         "B_1": 0.01,
-    #     },  # step-size depends on `L^tau` which requires knowledge of number of arms and is computed in the update
-    # },
+    {
+        "algo_name": "det_pg_entropy_multistage",
+        "algo_kwargs": {
+            "tau": 0.1,
+            "p": 1,
+            "B_1": 0.01,
+        },  # step-size depends on `L^tau` which requires knowledge of number of arms and is computed in the update
+    },
 ]
 
 run_experiment(
